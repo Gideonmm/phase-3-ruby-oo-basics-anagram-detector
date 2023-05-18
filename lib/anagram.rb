271eef32dab3e8b7e9ac2449dcfff71df19fc759
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-18 11:51:22 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      sorted_word == sort_letters(other_word)
+    end
+  
+    def sorted_word
+      @sorted_word ||= sort_letters(word)
+    end
+  
+    def sort_letters(str)
+      str.downcase.chars.sort.join
+    end
+  end
+  
